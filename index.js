59d47a777784c1a25e2c586b2028d0f3547a88dc
Kevin Haro <artcom_3@hotmail.com>
2024-01-24 13:28:21 -0700

    Create boxes using  API

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { createClient }  from 'redis';
+
+// Connect Redis
+const redisClient = createClient({
+  url: 'redis://localhost:6379'
+});
+redisClient.on('error', err => console.log('Redis Client Error', err));
+await redisClient.connect();
 
 const app = express();
 
-app.listen(3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-const boxes = [
-  {boxId:1},
-  {boxId:2},
-  {boxId:3},
-  {boxId:4},
-]
+app.post('/boxes/add', async (req, res, next) => {
+  console.log(req.body)
+  try {
+    // Get the existing boxes
+    let existingBoxes = await redisClient.json.get('boxes') || [];
+    
+    // const lastId = existingBoxes.slice(-1).id
 
-app.get('/boxes', (req, res, next) => {
-  res.send(JSON.stringify(boxes))
+    // Create a new box
+    const newBox = {
+      id: req.body.id
+    };
+
+    // Update the 'boxes' key in Redis with the new list of boxes
+    await redisClient.json.arrAppend('boxes', '$', newBox);
+
+    res.json({ success: true, message: 'Box created successfully', newBox });
+  } catch (error) {
+    console.error('Error creating box:', error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
+
+app.get('/boxes', async (req, res, next) => {
+  let boxes = await redisClient.json.get('boxes');
+  res.json(boxes);
 })
 
-console.log('Hello World')
+
+
+app.listen(3000, () => {
+  console.log('Listen on Port: 3000')
+});
